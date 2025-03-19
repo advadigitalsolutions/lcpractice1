1cd29bbb64ca96e7820648346904e616d4798335
Lee Zuvanich <leezuvanich@macbookair.lan>
2025-03-18 22:23:52 -0500

    Added final version of practicelaunchcode.js

---
+++ practicelaunchcode.js
@@ -0,0 +1,19 @@
+let custName = "Alice";
+let toyCar = 5;
+let carPrice = 20;
+let colorOfCar = " red";
+let toyDoll = 7;
+let dollPrice = 40;
+let colorOfDoll = " blue";
+let subTotal = custName + " bought " + toyCar + colorOfCar + " toy cars for $" + carPrice + ".";
+let finalTotal = custName + " bought " + toyDoll + colorOfDoll + " toy dolls for $" + dollPrice + ".";
+
+console.log(subTotal);
+console.log(finalTotal);
+
+/*
+Writing meaningful variable names helped me keep track of what I was doing as I made changes and thought out the operations.
+Common pitfalls to avoid when naming variables include using vague names, using names that are too long, and using names that are too similar to other variables.
+Clear variable names benefit team collaboration by making it easier for other team members to understand what the
+code is doing and what the variables are for.
+*/
